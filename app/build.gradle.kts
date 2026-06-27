0385bf54e3e428cba2f60431f4b6221793dd2a72
EmoScope <user@emoscope.app>
2026-06-27 16:48:10 +0800

    chore: 版本号更新至 v5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 6
-        versionName = "5.5"
+        versionName = "5.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
